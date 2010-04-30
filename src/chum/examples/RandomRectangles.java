0615614a44a2f25174dd42dbe54bbe5d7971a144
Jeremy Slade <jeremy@jkslade.net>
2010-04-30 12:54:45 -0600

    Cleaned up, added some documentation

--- src/chum/examples/RandomRectangles.java
+++ src/chum/examples/RandomRectangles.java
@@ -15,17 +15,30 @@ import javax.microedition.khronos.opengles.GL10;
 
 /**
    Draw random rectangles as fast as possible.  
+
+   On every frame, the logic tree updates a single rectangle with new random coordinates,
+   as well as a random color.  The render tree just clears the scene and draws that rectangle
+   (which is really a mesh of two triangles).
+
+   The draw rate should be limited by the hardware refresh cycle -- not likely to be more than
+   60Hz.  The frames-per-second rate (FPS) is displayed in both the title bar, and in a
+   Text object drawn on top of the rectangle.  This uses a special helper class called
+   {@link FPSNode} that automatically updates the text on a regular basis.
+
+   This example shows:
+   - How to set up a standard 2D projection
+   - How to update a mesh with dynamic vertices
+   - How to display the FPS with a Text object
 */
 public class RandomRectangles extends GameActivity
 {
     ColorNode colorNode;
     MeshNode quadNode;
-    Mesh quad;
     int[] verts = {
-        0,      0,      0,  // lower left
-        FP.ONE, 0,      0,  // lower right
-        FP.ONE, FP.ONE, 0,  // upper right
-        0,      FP.ONE, 0,  // upper left 
+        0, 0, 0,  // lower left
+        0, 0, 0,  // lower right
+        0, 0, 0,  // upper right
+        0, 0, 0,  // upper left 
     };
 
     FPSNode fpsNode;
@@ -72,23 +85,13 @@ public class RandomRectangles extends GameActivity
                 }
 
                 // The render tree consists of:
-                // - the root node is an orthographic (2D) projection, with
-                //   coordinates that range from [0,1] in both x and y.
-                // 
+                // - the root node is an orthographic (2D) projection
                 //   - ClearNode to clear the scene
                 //   - ColorNode to set the current draw color
                 //   - MeshNode to draw the mesh (just a two-triangle quad)
                 //   - TextNode to display the FPS
                 protected RenderNode createRenderTree() {
-                    Standard2DNode base = new chum.gl.render.Standard2DNode() {
-                            protected void setExtents(int width, int height) {
-                                left = 0f;
-                                right = 1f;
-                                bottom = 0f;
-                                top = 1f;
-                            }
-                        };
-
+                    Standard2DNode base = new chum.gl.render.Standard2DNode();
                     base.addNode(new ClearNode());
 
                     // Draw the rect
@@ -99,8 +102,7 @@ public class RandomRectangles extends GameActivity
 
                     // Draw the FPS text
                     fpsTextNode = new TextNode();
-                    fpsTextNode.setPosition(new Vec3(.1f,.1f,0f));
-                    fpsTextNode.setScale(FP.floatToFP(0.005f));
+                    fpsTextNode.setPosition(new Vec3(10f,10f,0f));
                     fpsTextNode.setColor(Color.RED);
                     base.addNode(fpsTextNode);
 
@@ -114,19 +116,6 @@ public class RandomRectangles extends GameActivity
                     Font font = new Font(renderContext,Typeface.DEFAULT,30);
                     fpsTextNode.setText(font.buildText("--"));
                     fpsNode.setText(fpsTextNode.text);
-
-
-//                     // TESTING:
-//                     try {
-//                         java.io.File file = new java.io.File
-//                             (android.os.Environment.getExternalStorageDirectory(),"font.png");
-//                         java.io.FileOutputStream out = 
-//                             new java.io.FileOutputStream(file);
-//                         fpsText.font.painter.bitmap.compress
-//                             (android.graphics.Bitmap.CompressFormat.PNG, 90, out);
-//                     } catch( Exception e ) {
-//                         e.printStackTrace();
-//                     }
                 }
             });
     }
@@ -134,13 +123,12 @@ public class RandomRectangles extends GameActivity
 
 
     protected Mesh createQuad() {
-        quad = new Mesh(true, false, true, 4, 4,
+        Mesh quad = new Mesh(true, false, true, 4, 4,
                         new VertexAttribute(VertexAttributes.Usage.Position));
         quad.setVertices(verts);
 
         short[] indices = { 0, 1, 2, 3 };
         quad.setIndices(indices);
-
         quad.type = GL10.GL_TRIANGLE_FAN;
 
         return quad;
@@ -148,8 +136,8 @@ public class RandomRectangles extends GameActivity
 
 
     protected void randomizeQuad() {
-        int x2 = GameController.random.nextInt(FP.ONE) + 1;
-        int y2 = GameController.random.nextInt(FP.ONE) + 1;
+        int x2 = GameController.random.nextInt(FP.intToFP(renderContext.width)) + 1;
+        int y2 = GameController.random.nextInt(FP.intToFP(renderContext.height)) + 1;
         int x1 = GameController.random.nextInt(x2);
         int y1 = GameController.random.nextInt(y2);
             
@@ -162,7 +150,7 @@ public class RandomRectangles extends GameActivity
         verts[9]  = x1;
         verts[10] = y2;
 
-        quad.setVertices(verts);
+        quadNode.mesh.setVertices(verts);
     }
 
 
@@ -174,6 +162,12 @@ public class RandomRectangles extends GameActivity
     }
 
 
+    /**
+       This method gets called from the game thread (from FPSNode), but
+       updateing the application titlebar has to be done in the UI thread,
+       because it involves changing the view.  So a Runnable has to be
+       posted to the UI thread to do the update
+    */
     protected void showFPSInTitle() {
         gameController.uiHandler.post(new Runnable(){
                 public void run() {
