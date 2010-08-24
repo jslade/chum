99f3f3ae67f940c5d63eca2f23f6a5ff91fe0155
Jeremy Slade <jeremy@jkslade.net>
2010-08-24 08:18:06 -0600

    ColorNode sets color on renderPrefix(), and restores color (by default to white) on renderPostfix()

--- chum-library/src/chum/gl/render/ColorNode.java
+++ chum-library/src/chum/gl/render/ColorNode.java
@@ -11,18 +11,37 @@ import javax.microedition.khronos.opengles.GL10;
 */
 public class ColorNode extends RenderNode {
 
-    /** The color to set */
+    /** The color to set on renderPrefix */
     public Color color;
 
 
+    /** The color to set on renderPostfix */
+    public Color postColor;
+    
+    
     public ColorNode(Color color) {
+        this(color,Color.WHITE);
+    }
+    
+    
+    public ColorNode(Color pre, Color post) {
         super();
-        this.color = color;
+        this.color = pre;
+        this.postColor = post;
     }
 
 
+    @Override
     public void renderPrefix(GL10 gl) {
-        gl.glColor4f(color.red,color.green,color.blue,color.alpha);
+        if ( color != null )
+            gl.glColor4f(color.red,color.green,color.blue,color.alpha);
+    }
+
+
+    @Override
+    public void renderPostfix(GL10 gl) {
+        if ( postColor != null )
+            gl.glColor4f(postColor.red,postColor.green,postColor.blue,postColor.alpha);
     }
 
 }
