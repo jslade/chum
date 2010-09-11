4d7cb6920d5487119be9c32d98f7b4f9dc220262
Jeremy Slade <jeremy@jkslade.net>
2010-09-10 19:32:49 -0600

    Added some transform helper methods

--- chum-library/src/chum/f/M4.java
+++ chum-library/src/chum/f/M4.java
@@ -5,25 +5,44 @@ package chum.f;
  */
 public class M4 {
     public float[] m = new float[16];
-    public static M4 xform = new M4();
+    private static M4 xform = new M4();
+    private static M4 tmp = new M4();
+    private static Vec3 vtmp = new Vec3();
+    
 	
     public M4() {
+        this(true);
     }
 	
+
+    public M4(boolean identity) {
+        if ( identity ) setIdentity();
+    }
+	
+
     public M4(M4 other) {
-        copy(other);
+        set(other);
     }
 
+    // TODO: deprecate this
     public final M4 copy(M4 other) {
+        return set(other);
+    }
+    
+    public final M4 set(M4 other) {
         for (int i = 0; i < 16; ++i)
                 m[i] = other.m[i];
         return this;
     }
 
     public final void multiply(Vec3 src, Vec3 dest) {
-        dest.x = src.x * m[0] + src.y * m[1] + src.z * m[2] + m[3];
-        dest.y = src.x * m[4] + src.y * m[5] + src.z * m[6] + m[7];
-        dest.z = src.x * m[8] + src.y * m[9] + src.z * m[10] + m[11];
+        synchronized(vtmp) {
+            if ( dest == src ) dest = vtmp;
+            dest.x = src.x * m[0] + src.y * m[1] + src.z * m[2] + m[3];
+            dest.y = src.x * m[4] + src.y * m[5] + src.z * m[6] + m[7];
+            dest.z = src.x * m[8] + src.y * m[9] + src.z * m[10] + m[11];
+            if ( dest == vtmp ) src.set(vtmp);
+        }
     }
 
 
@@ -63,6 +82,7 @@ public class M4 {
 
 
     public final void transpose(M4 dest) {
+        if ( dest == this ) dest = tmp;
         dest.m[0] = m[0];
         dest.m[1 ] = m[4];
         dest.m[2 ] = m[8];
@@ -79,6 +99,7 @@ public class M4 {
         dest.m[13] = m[7];
         dest.m[14] = m[11];
         dest.m[15] = m[15];
+        if ( dest == tmp ) this.set(dest);
     }
 
     
@@ -92,6 +113,16 @@ public class M4 {
     }
 
     
+    public final void scale(float x, float y, float z, M4 dest) {
+        synchronized(xform) {
+            if ( dest == this ) dest = tmp;
+            xform.scale(x,y,z);
+            this.multiply(xform,dest);
+            if ( dest == tmp ) this.set(dest);
+        }   
+    }
+
+    
     public final M4 scale(float scale) {
         clear();
         m[0] = scale;
@@ -102,12 +133,32 @@ public class M4 {
     }
 
     
+    public final void scale(float scale, M4 dest) {
+        synchronized(xform) {
+            if ( dest == this ) dest = tmp;
+            xform.scale(scale);
+            this.multiply(xform,dest);
+            if ( dest == tmp ) this.set(dest);
+        }   
+    }
+
+    
     public final M4 translate(Vec3 v) {
         setIdentity();
         m[3] = v.x;
         m[7] = v.y;
         m[11] = v.z;
         return this;
+    }
+
+    
+    public final void translate(Vec3 v, M4 dest) {
+        synchronized(xform) {
+            if ( dest == this ) dest = tmp;
+            xform.translate(v);
+            this.multiply(xform,dest);
+            if ( dest == tmp ) this.set(dest);
+        }   
     }
 
     
@@ -122,6 +173,16 @@ public class M4 {
     }
 
 
+    public final void rotate(Vec3 v, float theta, M4 dest) {
+        synchronized(xform) {
+            if ( dest == this ) dest = tmp;
+            xform.rotate(v,theta);
+            this.multiply(xform,dest);
+            if ( dest == tmp ) this.set(dest); 
+        }   
+    }
+
+    
     /**
        Rotate by a Quarternion
        (see Mat4::MakeHRot() from SVL)
@@ -168,6 +229,16 @@ public class M4 {
         m[15] = 1f;
 
         return this;
+    }
+
+    
+    public final void rotate(Vec4 q, M4 dest) {
+        synchronized(xform) {
+            if ( dest == this ) dest = tmp;
+            xform.rotate(q);
+            this.multiply(xform,dest);
+            if ( dest == tmp ) this.set(dest);
+        }   
     }
 
     
